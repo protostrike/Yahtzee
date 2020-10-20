81e5a93504b1048ef211017ab865b1472fc9059d
protostrike <alexmeng1014@gmail.com>
2020-10-20 14:07:39 -0400

    Refactored POM and test cases

--- src/test/java/ScoreCardTest.java
+++ src/test/java/ScoreCardTest.java
@@ -13,20 +13,30 @@ public class ScoreCardTest {
             {6,6,3,6,6},
             };
     enum scoreType {Ones, Twos, Threes, Fours, Fives, Sixes, ThreeOfAKind, FourOfAKind, FullHouse, SmallStraight, LargeStraight};
+
+    @Test
+    public void testUpperScoring(){
+        testOnes();
+        testTwos();
+        testThrees();
+        testFours();
+        testFives();
+        testSixes();
+    }
     public void testOnes(){
         int expectedScore;
         for(int[] dices : testDices){
             expectedScore = calculateScore(dices, scoreType.Ones);
-            sc.scoreOnes(dices);
+            sc.score(dices, 0);
             try{
-                Assert.assertEquals(expectedScore, sc.Ones);
+                Assert.assertEquals(expectedScore, sc.lowerSection[0]);
             }
             catch (AssertionError e){
                 System.out.println("Error when testing scoring in Ones\n"+
                         "Expected: " + expectedScore + "\n"+
-                        "Actual: " + sc.Ones);
+                        "Actual: " + sc.lowerSection[0]);
             };
-            sc.Ones = 0;
+            sc.lowerSection[0] = 0;
         }
         return;
     }
@@ -34,16 +44,16 @@ public class ScoreCardTest {
         int expectedScore;
         for(int[] dices : testDices){
             expectedScore = calculateScore(dices, scoreType.Twos);
-            sc.scoreTwos(dices);
+            sc.score(dices, 1);
             try{
-                Assert.assertEquals(expectedScore, sc.Twos);
+                Assert.assertEquals(expectedScore, sc.lowerSection[1]);
             }
             catch (AssertionError e){
                 System.out.println("Error when testing scoring in Twos\n"+
                         "Expected: " + expectedScore + "\n"+
-                        "Actual: " + sc.Twos);
+                        "Actual: " + sc.lowerSection[1]);
             };
-            sc.Twos = 0;
+            sc.lowerSection[1] = 0;
         }
         return;
     }
@@ -51,16 +61,16 @@ public class ScoreCardTest {
         int expectedScore;
         for(int[] dices : testDices){
             expectedScore = calculateScore(dices, scoreType.Threes);
-            sc.scoreThrees(dices);
+            sc.score(dices, 2);
             try{
-                Assert.assertEquals(expectedScore, sc.Threes);
+                Assert.assertEquals(expectedScore, sc.lowerSection[2]);
             }
             catch (AssertionError e){
                 System.out.println("Error when testing scoring in Threes\n"+
                         "Expected: " + expectedScore + "\n"+
-                        "Actual: " + sc.Threes);
+                        "Actual: " + sc.lowerSection[2]);
             };
-            sc.Ones = 0;
+            sc.lowerSection[2] = 0;
         }
         return;
     }
@@ -68,16 +78,16 @@ public class ScoreCardTest {
         int expectedScore;
         for(int[] dices : testDices){
             expectedScore = calculateScore(dices, scoreType.Fours);
-            sc.scoreFours(dices);
+            sc.score(dices, 3);
             try{
-                Assert.assertEquals(expectedScore, sc.Fours);
+                Assert.assertEquals(expectedScore, sc.lowerSection[3]);
             }
             catch (AssertionError e){
                 System.out.println("Error when testing scoring in Fours\n"+
                         "Expected: " + expectedScore + "\n"+
-                        "Actual: " + sc.Fours);
+                        "Actual: " + sc.lowerSection[3]);
             };
-            sc.Ones = 0;
+            sc.lowerSection[3] = 0;
         }
         return;
     }
@@ -85,16 +95,16 @@ public class ScoreCardTest {
         int expectedScore;
         for(int[] dices : testDices){
             expectedScore = calculateScore(dices, scoreType.Fives);
-            sc.scoreFives(dices);
+            sc.score(dices, 4);
             try{
-                Assert.assertEquals(expectedScore, sc.Fives);
+                Assert.assertEquals(expectedScore, sc.lowerSection[4]);
             }
             catch (AssertionError e){
                 System.out.println("Error when testing scoring in Fives\n"+
                         "Expected: " + expectedScore + "\n"+
-                        "Actual: " + sc.Fives);
+                        "Actual: " + sc.lowerSection[4]);
             };
-            sc.Ones = 0;
+            sc.lowerSection[4] = 0;
         }
         return;
     }
@@ -102,16 +112,16 @@ public class ScoreCardTest {
         int expectedScore;
         for(int[] dices : testDices){
             expectedScore = calculateScore(dices, scoreType.Sixes);
-            sc.scoreSixes(dices);
+            sc.score(dices, 5);
             try{
-                Assert.assertEquals(expectedScore, sc.Sixes);
+                Assert.assertEquals(expectedScore, sc.lowerSection[5]);
             }
             catch (AssertionError e){
                 System.out.println("Error when testing scoring in Sixes\n"+
                         "Expected: " + expectedScore + "\n"+
-                        "Actual: " + sc.Sixes);
+                        "Actual: " + sc.lowerSection[5]);
             };
-            sc.Sixes = 0;
+            sc.lowerSection[5] = 0;
         }
         return;
     }
@@ -142,5 +152,47 @@ public class ScoreCardTest {
     public void testChance(){
 
         return;
+    }
+
+    private int calculateScore(int[] dices, scoreType type){
+        int finalScore = 0;
+        switch(type){
+            case Ones:
+                for(int dice: dices){
+                    if(dice==1)
+                        finalScore += dice;
+                }
+                break;
+            case Twos:
+                for(int dice: dices){
+                    if(dice==2)
+                        finalScore += dice;
+                }
+                break;
+            case Threes:
+                for(int dice: dices){
+                    if(dice==3)
+                        finalScore += dice;
+                }
+                break;
+            case Fours:for(int dice: dices){
+                if(dice==4)
+                    finalScore += dice;
+            }
+                break;
+            case Fives:
+                for(int dice: dices){
+                    if(dice==5)
+                        finalScore += dice;
+                }
+                break;
+            case Sixes:
+                for(int dice: dices){
+                    if(dice==6)
+                        finalScore += dice;
+                }
+                break;
+        }
+        return finalScore;
     }
 }
