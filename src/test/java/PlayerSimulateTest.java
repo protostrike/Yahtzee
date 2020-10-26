618b536feeb4704044d4cce6ad7ea8a85f7e7af5
protostrike <alexmeng1014@gmail.com>
2020-10-26 16:07:04 -0400

    Added simulation test cases first, for simulating single and multiple players in a game

---
+++ src/test/java/PlayerSimulateTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PlayerSimulateTest {
+    static Launcher lc;
+
+    @Test
+    public void test(){
+        Assert.fail("Not implemented yet");
+    }
+
+    public void testOnePlayer(){
+
+    }
+
+    public void testTwoPlayers(){
+
+    }
+}
