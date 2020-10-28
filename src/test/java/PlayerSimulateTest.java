f7f5b125560c53be03cbb990953aae76e3509df8
protostrike <alexmeng1014@gmail.com>
2020-10-27 21:06:05 -0400

    Finished first version of simulation test cases

--- src/test/java/PlayerSimulateTest.java
+++ src/test/java/PlayerSimulateTest.java
@@ -2,7 +2,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class PlayerSimulateTest {
-    static Launcher lc;
+    static Launcher lcOne = new Launcher(1);
+    static Launcher lcTwo = new Launcher(2);
 
     @Test
     public void test(){
@@ -10,10 +11,22 @@ public class PlayerSimulateTest {
     }
 
     public void testOnePlayer(){
-
+        Player player = lcOne.players[0];
+        int roundCount = 1;
+        while(roundCount < 13){
+            lcOne.playRound(player);
+            roundCount++;
+        }
     }
 
     public void testTwoPlayers(){
-
+        Player P1 = lcTwo.players[0];
+        Player P2 = lcTwo.players[1];
+        int roundCount = 1;
+        while(roundCount < 13){
+            lcTwo.playRound(P1);
+            lcTwo.playRound(P2);
+            roundCount++;
+        }
     }
 }
