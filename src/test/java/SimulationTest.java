ccb81a660ce24ab4cf9902fbed62436c4cb5dc6b
protostrike <alexmeng1014@gmail.com>
2020-11-22 22:59:32 -0500

    Refactored test case to test ready status of player, and added a new one to test server ready status

--- src/test/java/SimulationTest.java
+++ src/test/java/SimulationTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.*;
@@ -13,37 +14,84 @@ public class SimulationTest {
     int port = 5000;
 
     @Test
-    public void test(){
-        testOnePlayer();
+    public void test() {
+        testOnePlayerReady();
+        testServerReady();
     }
 
-    public void testOnePlayer(){
+
+    //Test case: test player's ready status after one player connected to server
+    public void testOnePlayerReady() {
+        System.out.println("\nTest Case: Test Player Ready\n");
+        //Construct server and client
         s = new Server(5000);
+        c1 = new Client();
+
+        //Start server
+        new Thread(() -> s.start()).start();
+
+        //Start Client
+        new Thread(() -> c1.start()).start();
         try {
-            c1 = new Client(InetAddress.getLocalHost().getHostAddress(), 5000);
-            //Start server
-            Thread serverThread = new Thread(() -> {
-                s.start();
-            });
-            serverThread.start();
-            //Start Client
-            Thread clientThread = new Thread(() -> {
-                c1.start();
-            });
-            clientThread.start();
-
-            //Tell server that the game is ready to go
-            sendMessage("yes", c1);
-
-            //
-        } catch (UnknownHostException e) {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //Send ready message from client 1 to server
+        sendMessage("ready", c1);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertTrue(s.getConnection(1).isReady());
+        System.out.println("Test passed");
+        s.close();
+        c1.close();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    //Test case: test server's ready status after all three players are ready
+    public void testServerReady(){
+        System.out.println("\nTest Case: Test Server Ready\n");
+        //Construct server and client
+        s = new Server(5001);
+        c1 = new Client(5001);
+        c2 = new Client(5001);
+        c3 = new Client(5001);
+
+        //Start server
+        new Thread(() -> s.start()).start();
+        //Start client
+        new Thread(() -> c1.start()).start();
+        new Thread(() -> c2.start()).start();
+        new Thread(() -> c3.start()).start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Send ready messages from all three clients
+        sendMessage("ready", c1);
+        sendMessage("ready", c2);
+        sendMessage("ready", c3);
+
+        Assert.assertTrue(s.ready);
+        System.out.println("Test passed");
+        s.close();
+        c1.close();
+        c2.close();
+        c3.close();
+    }
+
     //Send given message from given client
-    private void sendMessage(String msg, Client c){
-        c.out.write(msg);
-        c.out.flush();
+    private void sendMessage(String msg, Client c) {
+        c.send(msg);
     }
 }
