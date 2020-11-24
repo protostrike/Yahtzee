73aa952bf75e0bfd8b1fd50741acab08ab4b7d7f
protostrike <alexmeng1014@gmail.com>
2020-11-24 00:20:02 -0500

    Refactored tests on waiting for certain conditions Added a test case for scoring one time after game is ready

--- src/test/java/SimulationTest.java
+++ src/test/java/SimulationTest.java
@@ -1,21 +1,16 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.*;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
-
 public class SimulationTest {
     Server s;
     Client c1;
     Client c2;
     Client c3;
-    int port = 5000;
 
     @Test
     public void test() {
         testOnePlayerReady();
+        sleep(1000);
         testServerReady();
     }
 
@@ -28,15 +23,18 @@ public class SimulationTest {
         c1 = new Client();
 
         //Start server
-        new Thread(() -> s.start()).start();
+        Thread server = new Thread(() -> s.start());
+        server.start();
 
         //Start Client
-        new Thread(() -> c1.start()).start();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread client1 = new Thread(() -> c1.start());
+
+        waitForServerUp();
+        client1.start();
+
+        //Wait until client is up and running
+        waitForOneClientUp(c1);
+
         //Send ready message from client 1 to server
         sendMessage("ready", c1);
         try {
@@ -46,13 +44,8 @@ public class SimulationTest {
         }
         Assert.assertTrue(s.getConnection(1).isReady());
         System.out.println("Test passed");
-        s.close();
-        c1.close();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        server.interrupt();
+        client1.interrupt();
     }
 
     //Test case: test server's ready status after all three players are ready
@@ -65,33 +58,135 @@ public class SimulationTest {
         c3 = new Client(5001);
 
         //Start server
-        new Thread(() -> s.start()).start();
-        //Start client
-        new Thread(() -> c1.start()).start();
-        new Thread(() -> c2.start()).start();
-        new Thread(() -> c3.start()).start();
+        Thread server = new Thread(() -> s.start());
+        server.start();
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        //Start client
+        Thread client1 = new Thread(() -> c1.start());
+        Thread client2 = new Thread(() -> c2.start());
+        Thread client3 = new Thread(() -> c3.start());
+
+        waitForServerUp();
+        client1.start();
+        client2.start();
+        client3.start();
+
+        waitForAllClientsUp();
 
         //Send ready messages from all three clients
         sendMessage("ready", c1);
         sendMessage("ready", c2);
         sendMessage("ready", c3);
 
-        Assert.assertTrue(s.ready);
+        //Wait until all three players are ready
+        waitForAllClientsReady();
+
+        Assert.assertTrue(s.isReady());
         System.out.println("Test passed");
-        s.close();
-        c1.close();
-        c2.close();
-        c3.close();
+
+        //Shut down thread after test
+        server.interrupt();
+        client1.interrupt();
+        client2.interrupt();
+        client3.interrupt();
+    }
+
+    public void testScoringFromOnePlayer(){
+        s = new Server(5002);
+        c1 = new Client(5002);
+        c2 = new Client(5002);
+        c3 = new Client(5002);
+
+        //Start server
+        Thread server = new Thread(() -> s.start());
+        server.start();
+
+        //Start client
+        Thread client1 = new Thread(() -> c1.start());
+        Thread client2 = new Thread(() -> c2.start());
+        Thread client3 = new Thread(() -> c3.start());
+
+        waitForServerUp();
+        client1.start();
+        client2.start();
+        client3.start();
+
+        waitForAllClientsUp();
+
+        //Send ready messages from all three clients
+        sendMessage("ready", c1);
+        sendMessage("ready", c2);
+        sendMessage("ready", c3);
+
+        //Wait until all three players are ready
+        waitForAllClientsReady();
+
+        //Send a message from client 1 to score in Ones
+        sendMessage("Category: 1, Dices: [1,1,1,1,1]", c1);
+        sleep(2000);
+
+        Assert.assertEquals(s.card.getScoreByCategory(1), 5);
+        Assert.assertFalse(c1.scorableCategory[0]);
+    }
+
+    private void sleep(long time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     //Send given message from given client
     private void sendMessage(String msg, Client c) {
         c.send(msg);
+    }
+
+    private void waitForServerUp(){
+        System.out.println("Wait until server is up and running properly");
+        while(!s.up){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Still waiting...");
+        }
+    }
+
+    private void waitForOneClientUp(Client c){
+        System.out.println("Wait until the client is up and running properly");
+        while(!c.up){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Still waiting...");
+        }
+    }
+
+    private void waitForAllClientsUp(){
+        System.out.println("Wait until all clients are up and running properly");
+        while(!c1.up||!c2.up||!c3.up){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Still waiting...");
+        }
+    }
+
+    private void waitForAllClientsReady(){
+        System.out.println("Wait until all clients are ready to play");
+        while(!s.getConnection(1).isReady()||!s.getConnection(2).isReady()||!s.getConnection(3).isReady()){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Still waiting...");
+        }
     }
 }
